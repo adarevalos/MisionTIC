a1852cf9f4d76082875c2e58f5e3373c6fec0f55
adarevalos <adarevalos@unal.edu.co>
2021-07-13 17:21:50 -0500

    main in clasetres

--- src/clasetres1julio.java
+++ src/clasetres1julio.java
@@ -3,5 +3,8 @@ En esta clase se inicia con los pasos para utilizar GITHUB y vincular git con Vi
 https://www.youtube.com/watch?v=jObkh6ju9cM&ab_channel=iMaster-Educaci%C3%B3nqueinspira
 */
 public class clasetres1julio {
+    public static void main(String[] args) throws Exception {
     
+    
+    }   
 }
